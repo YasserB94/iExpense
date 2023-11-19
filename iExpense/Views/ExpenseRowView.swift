0e1185cd7ee1374377849bfd59dda5830e4b09a2
Yasser Bal <yasserbal@icloud.com>
2023-11-19 13:47:17 +0100

    Adding view to display 1 expense

--- iExpense/Views/ExpenseRowView.swift
+++ iExpense/Views/ExpenseRowView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ExpenseRowView: View {
+    let expense:Expense
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading) {
+                Text(expense.name)
+                    .font(.headline)
+                Text(expense.type.description.capitalized)
+                
+                    .font(.caption)
+            }
+            Spacer()
+            Text(
+                expense.amount,
+                format:.currency(
+                    code:Locale.current.currency?.identifier ?? "EUR"
+                )
+            )
+            .foregroundColor(
+                getExpenseColor(expense: expense)
+            )
+        }
+    }
+    
+    private func getExpenseColor(expense:Expense) -> Color
+    {
+        switch expense.amount {
+        case 100...:
+            return .red
+        case 10...:
+            return .orange
+        default:
+            return .primary
+        }
     }
 }
-
 struct ExpenseRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ExpenseRowView()
+        ExpenseRowView(expense: Expense(name: "Preview", type: .personal, amount: 5.00))
     }
 }
